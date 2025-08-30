d7015098a381ca899aaf17c6999256bd5a40d75a
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-08-30 19:23:37 +0530

    break 3

---
+++ week2/week2.2/4_Promisied.js
@@ -0,0 +1,13 @@
+function setTimeoutPromisified(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, time)
+    } )
+}
+
+
+function someFunction(){
+    console.log("Call me after the setTimeoutPromisified function is done")
+}
+
+const p = setTimeoutPromisified(5000).then(someFunction)   // returns objects of Promis
+console.log(p)
