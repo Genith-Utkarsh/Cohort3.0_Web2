75243cc2ac9133c5fe0782f9d89f19d885f24312
Utkarsh Krishnat Buva <buvautkarsh849@gmail.com>
2025-09-07 21:45:39 +0530

    added some part

--- week4/week4.2/1_calculateSum.js
+++ week4/week4.2/1_calculateSum.js
@@ -7,6 +7,10 @@ function totalSum(num){
     return ( num * (num + 1) / 2)
 }
 
+function add(a, b){
+    return a + b
+}
+
 app.get("/sum", (req, res) => {
     const num = Number(req.query.num)
 
@@ -14,6 +18,19 @@ app.get("/sum", (req, res) => {
         number : num,
         result :  totalSum(num)
     })
+})
+
+
+app.get("/add", (req, res)=> {
+    const a = Number(req.query.a)
+    const b = Number(req.query.b)
+
+    const result = add(a, b)
+
+    res.status(200).json({
+        message : `Result of adding ${a} and ${b} is ${result}`
+    })
+
 })
 
 app.listen(PORT, function(){
